7c18d339b6ec6b5ecddb1b7933684ef350c16737
Denis Smirnov <w1png@yandex.ru>
2024-09-18 22:53:28 +0300

    Add some components

---
+++ src/app/api/main/[trpc]/route.ts
@@ -0,0 +1,30 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import type { NextRequest } from "next/server";
+
+import { env } from "~/env";
+import { router } from "~/server/api/main";
+import { createTRPCContext } from "~/server/api/trpc";
+
+const createContext = (req: NextRequest) => {
+  return createTRPCContext({
+    headers: req.headers,
+  });
+};
+
+const handler = (req: NextRequest) =>
+  fetchRequestHandler({
+    endpoint: "/api/main",
+    req,
+    router: router,
+    createContext: () => createContext(req),
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            );
+          }
+        : undefined,
+  });
+
+export { handler as GET, handler as POST };
