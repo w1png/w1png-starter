6f9adb399fe60a17be27e5b298b35e724956a00b
Denis Smirnov <w1png@yandex.ru>
2024-12-01 15:35:53 +0300

    Fix file

--- src/server/api/routers/file.ts
+++ src/server/api/routers/file.ts
@@ -1,6 +1,5 @@
 import { TRPCError } from "@trpc/server";
 import { eq } from "drizzle-orm";
-import { z } from "zod";
 import { FileSchema } from "~/lib/shared/types/file";
 import { IdSchema } from "~/lib/shared/types/utils";
 import {
@@ -12,48 +11,27 @@ import { files } from "~/server/db/schema";
 
 export const fileRouter = createTRPCRouter({
   create: protectedProcedure
-    .input(
-      FileSchema.merge(
-        z.object({
-          createdById: z
-            .string({
-              required_error: "Необходимо указать идентификатор автора",
-              invalid_type_error: "Неверный тип идентификатора автора",
-            })
-            .min(1, "Необходимо указать идентификатор автора"),
-          ticketId: z
-            .string({
-              required_error: "Необходимо указать идентификатор обращения",
-              invalid_type_error: "Неверный тип идентификатора обращения",
-            })
-            .min(1, "Необходимо указать идентификатор обращения"),
-          messageId: z
-            .string({
-              invalid_type_error: "Неверный тип идентификатора сообщения",
-            })
-            .optional(),
-        }),
-      ),
-    )
+    .input(FileSchema)
     .mutation(async ({ ctx, input }) => {
       const s3Key = await ctx.s3.upload(input);
 
-      await ctx.db.insert(files).values({
-        ...input,
-        id: undefined,
-        objectId: s3Key,
-      });
+      const [file] = await ctx.db
+        .insert(files)
+        .values({
+          ...input,
+          id: undefined,
+          objectId: s3Key,
+          createdById: ctx.session.user.id,
+        })
+        .returning();
 
       return {
-        key: s3Key,
+        id: file!.id,
       };
     }),
   get: publicProcedure.input(IdSchema).query(async ({ ctx, input }) => {
     const file = await ctx.db.query.files.findFirst({
       where: eq(files.id, input.id),
-      with: {
-        ticket: true,
-      },
     });
 
     if (!file) {
