6a9c7a14f7d1c2dd0ad371560df1314b39d24e0d
Denis Smirnov <w1png@yandex.ru>
2025-01-09 05:40:43 +0300

    Remove test page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,14 +1,10 @@
-import Bun from "bun";
-
 import { headers } from "next/headers";
 import { auth } from "~/server/auth/auth";
-import Upload from "./upload";
 
 export default async function LandingPage() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  return <Upload />;
   return JSON.stringify(session);
 }
