33a952d8a3b0db0c523bfbf93a19dec057ba0464
Denis Smirnov <w1png@yandex.ru>
2025-04-16 13:06:07 +0300

    Move to bun redis

--- src/server/redis/index.ts
+++ src/server/redis/index.ts
@@ -1,17 +1,1 @@
-import { createClient } from "redis";
-import { env } from "~/env";
-
-const globalForRedis = globalThis as unknown as {
-  redis: ReturnType<typeof createClient> | undefined;
-};
-
-if (!globalForRedis.redis) {
-  const redis = createClient({ url: env.REDIS_URL });
-  globalForRedis.redis = redis;
-  redis.on("error", (err) => {
-    console.error("Redis Client Error", err);
-  });
-  await redis.connect();
-}
-
-export const redis = globalForRedis.redis;
+export const redis = Bun.redis;
--- src/server/s3/index.ts
+++ src/server/s3/index.ts
@@ -1,14 +1,12 @@
-import { S3Client } from "bun";
-
 import { env } from "~/env";
 
 const globalForS3 = globalThis as unknown as {
-  s3: S3Client | undefined;
+  s3: typeof Bun.S3Client | undefined;
 };
 
 export const s3 =
   globalForS3.s3 ??
-  new S3Client({
+  new Bun.S3Client({
     region: env.S3_REGION,
     endpoint: env.S3_ENDPOINT,
     accessKeyId: env.S3_ACCESS_KEY,
