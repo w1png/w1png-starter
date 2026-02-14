26720dd6aa1f7fcef47e523fb1f279f4a16bf005
Denis Smirnov <w1png@yandex.ru>
2026-02-14 23:34:32 +0300

    Add ln-env.sh

---
+++ ln-env.sh
@@ -0,0 +1,4 @@
+rm ./apps/server/.env && ln .env ./apps/server/.env
+rm ./apps/web/.env && ln .env ./apps/web/.env
+rm ./packages/telegram/.env && ln .env ./packages/telegram/.env
+rm ./packages/database/.env && ln .env ./packages/database/.env
